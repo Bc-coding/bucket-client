7e06c30652141879de21e795e0b38e47ecc536c0
Belle Carrie <bc.coding@hotmail.com>
2023-02-02 18:42:06 +0000

    added login react-hook-form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,14 +18,46 @@ import {
 } from "@chakra-ui/react";
 import { FaUserAlt, FaLock } from "react-icons/fa";
 import Layout from "../components/Layout";
+import { useForm } from "react-hook-form";
+import { gql, useQuery, useLazyQuery } from "@apollo/client";
+import { Redirect } from "react-router";
 
 const CFaUserAlt = chakra(FaUserAlt);
 const CFaLock = chakra(FaLock);
 
 const Login = () => {
+  // Use form state
+  const { values, handleChange, handleSubmit } = useForm(
+    credentials => loginUser(),
+    {
+      email: "",
+      password: "",
+    }
+  );
+
   const [showPassword, setShowPassword] = useState(false);
 
   const handleShowClick = () => setShowPassword(!showPassword);
+
+  // Lazy query for login user method
+  const [loginUser, { called, loading, data, error }] = useLazyQuery(
+    LOGIN_USER,
+    { variables: values }
+  );
+
+  // Wait for lazy query
+  if (called && loading) return <div>Loading...</div>;
+
+  // Show error message if lazy query fails
+  if (error) return <div>{error.message}</div>;
+
+  // Store token if login is successful
+  if (data) {
+    window.localStorage.setItem("token", data.loginUser.token);
+
+    // Redirect to home page
+    return <Redirect to="/" />;
+  }
 
   return (
     <Layout>
@@ -45,7 +77,7 @@ const Login = () => {
           <Avatar bg="teal.500" />
           <Heading color="teal.400">Welcome</Heading>
           <Box minW={{ base: "90%", md: "468px" }}>
-            <form>
+            <form onSubmit={handleSubmit}>
               <Stack spacing={4} p="1rem" boxShadow="md">
                 <FormControl>
                   <InputGroup>
@@ -53,7 +85,16 @@ const Login = () => {
                       pointerEvents="none"
                       children={<CFaUserAlt color="gray.300" />}
                     />
-                    <Input type="email" placeholder="email address" />
+                    <Input
+                      required
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder="email address"
+                      value={values.email}
+                      onChange={handleChange}
+                      autoFocus
+                    />
                   </InputGroup>
                 </FormControl>
                 <FormControl>
@@ -64,8 +105,13 @@ const Login = () => {
                       children={<CFaLock color="gray.300" />}
                     />
                     <Input
+                      required
+                      id="password"
+                      name="password"
                       type={showPassword ? "text" : "password"}
                       placeholder="Password"
+                      value={values.password}
+                      onChange={handleChange}
                     />
                     <InputRightElement width="4.5rem">
                       <Button h="1.75rem" size="sm" onClick={handleShowClick}>
