cdb5927a718880e2b38cb6490444b609d2052381
Belle Carrie <bc.coding@hotmail.com>
2023-02-22 15:09:55 +0000

    added details to popost

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Header from "./header/Header";
 import { Container } from "@chakra-ui/react";
+import Footer from "./footer";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <Container maxW="container.lg" marginTop="80px">
+      <Container maxW="container.md" marginTop="80px">
         {children}
       </Container>
+      {/* <Footer /> */}
     </>
   );
 };
